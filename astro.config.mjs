8fc1c813762b2f8111759fac66d0993c0fcf0e72
changhoetyng <hoetyng@gmail.com>
2024-07-27 00:32:00 +0100

    added contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from "astro/config";
 import { fileURLToPath, URL } from "node:url";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), mdx()],
   vite: {
     resolve: {
       alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
-  },
+        "@": fileURLToPath(new URL("./src", import.meta.url))
+      }
+    }
+  }
 });
